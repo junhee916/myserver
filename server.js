684b5e60464159f0a72a7c6119e7723efb6c05f5
junhee916 <junhee916@naver.com>
2019-10-07 22:03:52 +0900

    last version

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
 const express = require("express")
+const app = express()
+
+app.get("/", function(req, res){
+    res.send("<h1>main page!</h1>")
+})
+
+app.listen(80, function(){
+    console.log("PORT 80 OPEND")
+})
